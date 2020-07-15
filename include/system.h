d2be8661e5b45cf19cd25e5908727234780ec406
LucasSilicon <lucas.sabbatini@siliconlife.ai>
2020-07-15 11:05:30 -0300

    Minor changes

--- include/system.h
+++ include/system.h
@@ -9,16 +9,16 @@
 
 class System {
  public:
-  Processor& Cpu() { return _cpu; }                  // TODO: See src/system.cpp
-  std::vector<Process>& Processes() { return _processes; }  // TODO: See src/system.cpp
-  float MemoryUtilization();          // TODO: See src/system.cpp DONE
-  long UpTime();                      // TODO: See src/system.cpp
-  int TotalProcesses();               // TODO: See src/system.cpp
-  int RunningProcesses();             // TODO: See src/system.cpp
-  std::string Kernel() const { return _kernel; }               // TODO: See src/system.cpp
-  std::string OperatingSystem() const { return _operating_system; }      // TODO: See src/system.cpp
+  Processor& Cpu() { return _cpu; }
+  std::vector<Process>& Processes() { return _processes; }
+  float MemoryUtilization();
+  long UpTime();
+  int TotalProcesses();
+  int RunningProcesses();
+  std::string Kernel() const { return _kernel; }
+  std::string OperatingSystem() const { return _operating_system; }
   System();
-  // TODO: Define any necessary private members
+  
  private:
   Processor _cpu = {};
   std::vector<Process> _processes = {};
